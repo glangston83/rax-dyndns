c2edd2659b2c7d82d58b8c8ad29ff4cac1ff912c
Greg Langston <glangston83@gmail.com>
2013-10-07 21:45:38 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ rax-dyndns
 ==========
 
 Dynamic DNS using pyrax and argparse -- publish server's public IP to subdomain 
+
+Requirements: 
+.rackspace_cloud_credentials
+[rackspace_cloud]
+username = <rackspace username>
+api_key = <rackspace API key>
+
+Usage: 
+./dyndns.py --subdomain <subdomain>
---
+++ rax-dyndns.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import pyrax
+import urllib2
+import datetime
+import argparse
+import logging
+
+logging.basicConfig(filename='rax-dyndns.log',level=logging.DEBUG)
+
+now = datetime.datetime.now()
+format_date = now.strftime("%y-%m-%d %H:%M")
+
+
+pyrax.settings.set('identity_type', 'rackspace')
+pyrax.set_credential_file(".rackspace_cloud_credentials")
+
+#acquire public IP of home server
+my_ip = urllib2.urlopen('http://ip.42.pl/raw').read()
+
+#hook dns
+dns = pyrax.cloud_dns
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+#    parser.add_argument("--source", required = True, help = "The Account Number of the Source AWS Account for migration")
+    #parser.add_argument("--domain", required = True, dest="domain", help = "Domain to update record on")
+    parser.add_argument("--subdomain", required = True, dest="subdomain", help = "subdomain to update record on")
+    parser.add_argument("--debug", help = "Turn on boto debugging", dest = "debug", default = False, action = "store_true")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    options = parse_arguments()
+
+    if options.debug:
+        boto.set_stream_logger('boto')
+
+    subdom = options.subdomain
+    fqdn = subdom.split(".")
+    dom = fqdn[1]+"."+fqdn[2]
+
+
+#acquire domains on account in list and set to public IP of home server
+    domains = dns.list()
+    for domain in domains:
+        if domain.name == dom:
+            logging.debug(domain)
+            recs = dns.list_records(domain)
+            rec_found=False
+            for rec in recs:
+                if rec.name == subdom:
+                    rec_found=True
+                    #print format_date, "-- Updating record for ", rec.name, "IP Address ", my_ip
+                    logging.debug(str(format_date) + "-- Updating record for " + str(rec.name) + " IP Address " + str(my_ip))
+                    try:
+                        rec.update(data=my_ip)
+                    except e:
+                        logging.error(e)
+            if not rec_found:
+                #print "Record not found for %s" % subdomain
+                logging.debug("Record not found for %s" % subdomain)
+
